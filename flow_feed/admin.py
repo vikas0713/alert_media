d6ec10d0db8c1498fd6b4da0657770c97e8f7986
vikas <vikas0713@gmail.com>
2016-09-11 09:03:02 +0530

    updated admin.py

--- flow_feed/admin.py
+++ flow_feed/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from flow_feed.models import Posts
+from flow_feed.models import Posts, Votes
 
 admin.site.register(Posts)
+admin.site.register(Votes)
